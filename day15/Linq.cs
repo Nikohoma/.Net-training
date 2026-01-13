d287a3872b53f8ee0c7b8883a3109dd061c48a36
Nikohoma <nikhiljr0207@gmail.com>
2026-01-13 16:15:02 +0530

    day15 added

---
+++ day15/Linq.cs
@@ -0,0 +1,116 @@
+﻿namespace Classes;
+
+public class Linq
+{
+	// Constructor
+	public Linq()
+	{
+
+	}
+
+	public void LinqEx()
+	{
+		string[] names = { "A", "B", "C" };
+
+		// Normal way
+		foreach(var i in names)
+		{
+			if (i == "B") { Console.WriteLine($"B is present"); }
+		}
+
+		// Linq 
+		Console.Write("Linq Equivalent: ");
+		var findName = from i in names
+					   where i == "B"
+					   select i;
+		if (findName != null) { Console.WriteLine("B is present"); }
+
+		// Order by
+        var ordered = from name in names
+                      orderby name descending
+                      select name.ToLower();  //can take custom function / objects as well
+
+        Console.WriteLine("Ordered List (Descending): ");
+		foreach (var o in ordered) { Console.Write(o + " "); }
+		Console.WriteLine();
+	}
+
+	/// <summary>
+	/// Palindrome check method. can be pass to linq queries
+	/// </summary>
+	/// <param name="input"></param>
+	/// <returns></returns>
+	public string IsPalindrome(string input)
+	{
+		int l = 0; int r = input.Length - 1;
+
+		while (l < r)
+		{
+			if (input[l] != input[r] ) { return "Not Palindrome"; }
+			else { l++; r--; }
+		}
+		return "Palindrome";
+	}
+
+	/// <summary>
+	/// To print all Processes
+	/// </summary>
+	public static void LinqEx1()
+	{
+		var processes = from p in System.Diagnostics.Process.GetProcesses()          // Type: Process Enumeration
+						select new ClassForLinq(p.ProcessName, p.Id);
+
+		foreach(var p in processes)
+		{
+			Console.WriteLine($"Process Id : {p.Id} , Process Name : {p.Name}");
+		}
+	}
+
+	/// <summary>
+	/// Anonymous DataType
+	/// </summary>
+    public static void LinqEx2()
+    {
+        var processes = from p in System.Diagnostics.Process.GetProcesses()          
+                        select new { p.ProcessName, p.Id };                         // Microsoft automatically creates a class. objects can be accessed inside the function only. ProcessName and Id are the property of anonymous object(created at backend)
+
+        foreach (var p in processes)
+        {
+            Console.WriteLine($"Process Id : {p.Id} , Process Name : {p.ProcessName}");
+        }
+		
+    }
+
+    public static void LinqEx3()
+    {
+		var MaxProcess = System.Diagnostics.Process.GetProcesses().Max(p => p.Id);     // Linq with Lambda Expression
+		Console.WriteLine($"Max Process Id: {MaxProcess}");
+    }
+
+
+}
+
+public class ClassForLinq
+{
+    public string Name; public int Id;
+
+    public ClassForLinq(string Name, int Id)
+    {
+        this.Name = Name; this.Id = Id;
+    }
+}
+
+
+public class ClassRoom
+{
+	public int Id;
+	public decimal Mark1 { get; set; }
+	public decimal Mark2 { get; set; }
+
+	public ClassRoom(int Id,decimal Mark1, decimal Mark2)
+	{
+		this.Id = Id; this.Mark1 = Mark1; this.Mark2 = Mark2;
+	}
+
+	
+}
---
+++ day15/Main.cs
@@ -0,0 +1,68 @@
+﻿namespace MainClass;
+
+using Classes;
+
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Linq l = new Linq();
+        l.LinqEx();
+        Console.WriteLine(l.IsPalindrome("AJA"));
+
+
+
+        string[] names = { "AJA", "BAB", "CKL" };
+
+        Console.WriteLine("\nLinq Palindrome: ");
+        var newLinqObjects = from n in names
+                             select l.IsPalindrome(n);
+
+        foreach (var x in newLinqObjects)
+        {
+            Console.WriteLine(x);
+        }
+
+
+        Console.WriteLine("\nProcess with Linq");
+        Linq.LinqEx1();
+
+        Linq.LinqEx3();
+
+        #region Linq on Generics  (reger Notes of Generics + LINQ)
+        List<ClassRoom> StudentList = new List<ClassRoom>
+        {
+            new ClassRoom(1,23,43),
+            new ClassRoom(2,34,23),
+            new ClassRoom(3,21,34),
+            new ClassRoom(4,23,12)
+        };
+
+        Console.WriteLine("\nMaximum Average: ");
+        var maxAverage = StudentList.Max(s => (s.Mark1 + s.Mark2) / 2);
+        Console.WriteLine(maxAverage);
+
+        var top = StudentList.Select(s => new { Average = (s.Mark1 + s.Mark2) / 2 }).OrderByDescending(x => x.Average).FirstOrDefault();      
+        Console.WriteLine(top);
+
+        // List Containing ID and corresponding average of marks
+        var averages = StudentList.Select(s => new {Average = (s.Mark1 + s.Mark2) / 2 });  // s = Anonymous object
+        Console.WriteLine(averages);
+        foreach (var a in averages) { Console.Write(a); }
+        #endregion
+
+        #region Linq on Collections // Not suitable for linq since collections take input in the form of objects.
+        Stack<ClassRoom> myStack = new Stack<ClassRoom>();
+        myStack.Push(new ClassRoom(11,23,43));
+        myStack.Push(new ClassRoom(12, 33, 42));
+        myStack.Push(new ClassRoom(13, 83, 53));
+
+        var AverageStack = myStack.Select(a => new { Average = (a.Mark1 + a.Mark2) / 2 } ).OrderByDescending(x=>x.Average).Skip(1).First();
+        Console.WriteLine("\nSecond maximum Average of Stack : ");
+        Console.WriteLine(AverageStack);
+        
+        #endregion
+
+    }
+}
