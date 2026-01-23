98ed4f6db2b43c6952b0b89349d9f88053c16fdf
Nikohoma <nikhiljr0207@gmail.com>
2026-01-23 11:50:53 +0530

    day 20 added

---
+++ day20/MainClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Attributes;
+
+namespace MainClass
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine(c.OldAdd(2,3));  // Attribute Warning
+            Console.WriteLine(c.NewAdd(2,3));
+        }
+    }
+}
---
+++ day20/attributes.cs
@@ -0,0 +1,21 @@
+﻿// Attributes are used to change the metadata
+
+namespace Attributes
+{
+    public class Calculator
+    {
+        // This attribute will generate a compiler warning
+        [Obsolete("Use the NewAdd() Function")]
+        public int OldAdd(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int NewAdd(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+
+}
