66365c1dafa51d008c118202f97f780818295524
Nikohoma <nikhiljr0207@gmail.com>
2026-01-26 19:32:18 +0530

    Topbrains Question8 added

---
+++ TopBrains/Banktransactionprogram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question8
+{
+    /// <summary>
+    /// Class with a method to transact from the balance
+    /// </summary>
+    public class Banktransactionprogram
+    {
+        /// <summary>
+        /// Member method to update balance based on the transactions
+        /// </summary>
+        /// <param name="balance"></param>
+        /// <param name="transactions"></param>
+        /// <returns></returns>
+        public int Transactions(int balance, int[] transactions)
+        {
+            int accountBalance = balance;
+            foreach(var t in transactions)
+            {
+                if (accountBalance > t) { accountBalance += t; }
+            }
+            return accountBalance;
+        }
+    }
+
+    /// <summary>
+    /// Main Class
+    /// </summary>
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Transactions list (+ve for deposit , -ve for withdraw)
+            int[] transactions = [250, 10, -59, -144, 90];
+
+            Banktransactionprogram b = new Banktransactionprogram();  // Instance Create
+            Console.WriteLine(b.Transactions(25000, transactions));   // Calling Method
+        } 
+        
+    }
+}
