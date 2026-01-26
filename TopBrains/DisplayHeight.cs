ac2d39f1e2873b6c2c16a3c7f9ef5a386913e629
Nikohoma <nikhiljr0207@gmail.com>
2026-01-26 19:04:38 +0530

    Topbrains questions added

---
+++ TopBrains/DisplayHeight.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question7
+{
+    /// <summary>
+    /// Class having a method to categorise the height
+    /// </summary>
+    public class DisplayHeight
+    {
+        /// <summary>
+        /// Member Method to categorise the height in three categories
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public string HeightCategory(int height)
+        {
+            return height switch            // Switch to categorise height in 3 categories
+            {
+                < 150 => "Short",
+                <= 180 => "Average",
+                >= 180 => "Tall"
+
+            };
+        }
+    }
+
+    /// <summary>
+    /// Main Class
+    /// </summary>
+    public class MainClass
+    {
+        /// <summary>
+        /// Entry Point
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            //User Input
+            Console.WriteLine("Enter height in cms: ");
+            int height = int.Parse(Console.ReadLine());
+
+            // Instance of the class
+            DisplayHeight h = new DisplayHeight();
+            Console.WriteLine(h.HeightCategory(height)); // Method calling
+        }
+    }
+}
+
--- TopBrains/LargestInteger.cs
+++ TopBrains/LargestInteger.cs
@@ -4,13 +4,23 @@ using System.Text;
 
 namespace LargestInteger
 {
+    /// <summary>
+    /// Class incorporating the Method to find the Largest Integer among the inputs 
+    /// </summary>
     public class Question6
     {
+        /// <summary>
+        /// Member method to find the largest integer among the inputs.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
         public int LargestInteger(int a, int b, int c)
         {
-            if (a > b && a > c) { return a; }
-            else if (b > a && b > c) { return b; }
-            else return c;
+            if (a > b && a > c) { return a; }         // Case1: a is largest
+            else if (b > a && b > c) { return b; }    // Case2: b is largest
+            else return c;                            // Case3: c is largest
         }
     }
 
@@ -19,6 +29,7 @@ namespace LargestInteger
     {
         public static void Main(string[] args)
         {
+            // Input from the user
             Console.WriteLine("\nInput First Number: ");
             int num1= int.Parse(Console.ReadLine());
             Console.WriteLine("\nInput Second Number: ");
@@ -26,7 +37,9 @@ namespace LargestInteger
             Console.WriteLine("\nInput Third Number: ");
             int num3 = int.Parse(Console.ReadLine());
 
+            // Class Instance
             Question6 c = new Question6();
+            // Calling the method
             Console.WriteLine($"\nLargest Number : {c.LargestInteger(num1, num2, num3)}");
         }
     }
