afbf83388cef1078b1aa53b414fbea581934ff7d
Nikohoma <nikhiljr0207@gmail.com>
2026-02-15 22:23:56 +0530

    Questions added

---
+++ day31-PracticeQuestions/MedicineInventory.cs
@@ -0,0 +1,159 @@
+﻿namespace MedicineInventory
+{
+    public class Medicine
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int ExpiryYear { get; set; }
+
+        public Medicine()
+        {
+
+        }
+    }
+
+    public class InvalidPriceException : Exception
+    {
+        public InvalidPriceException(string message) : base(message)
+        {
+
+        }
+    }
+    public class InvalidExpiryYear : Exception
+    {
+        public InvalidExpiryYear(string message) : base(message)
+        {
+
+        }
+    }
+    public class DuplicateMedicineException : Exception
+    {
+        public DuplicateMedicineException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class MedicineNotFoundException : Exception
+    {
+        public MedicineNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class MedicineUtility
+    {
+        private SortedDictionary<string, Medicine> _meds = new SortedDictionary<string, Medicine>();
+
+        public void AddMedicine(Medicine medicine)
+        {
+            if (medicine.Price < 0)
+            {
+                throw new InvalidPriceException("Invalid Price. Price must be more than 0.");
+            }
+            if (medicine.ExpiryYear < int.Parse(DateTime.Now.ToString("yyyy")))
+            {
+                throw new InvalidExpiryYear("Expiry Year Invalid.");
+            }
+            foreach (var m in _meds)
+            {
+                if (medicine.Id == m.Value.Id)
+                {
+                    throw new DuplicateMedicineException("Medicine already added.");
+                }
+            }
+            _meds.Add(medicine.Id, medicine);
+            Console.WriteLine("Medicine Added Successfully.");
+        }
+
+        public Dictionary<string,Medicine> GetAllMedicines()
+        {
+            if (_meds.Count == 0) { Console.WriteLine("No Medicines in stock."); }
+            return _meds.OrderBy(m => m.Value.ExpiryYear).ToDictionary(m=>m.Key,m=>m.Value);
+        }
+
+        public void UpdateMedicinePrice(string id, int newPrice)
+        {
+            foreach (var m in _meds)
+            {
+                if (m.Key == id)
+                {
+                    m.Value.Price = newPrice;
+                    Console.WriteLine("Price Updated.");
+                    return;
+                }
+            }
+            throw new MedicineNotFoundException("Could not found the specified medicine.");
+        }
+    }
+
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MedicineUtility mu = new MedicineUtility();
+            bool flag = true;
+
+            Console.WriteLine("=========================");
+            Console.WriteLine("===========MENU==========");
+            Console.WriteLine("=========================");
+            Console.WriteLine("1. Display All Medicines (sorted by expiry year).");
+            Console.WriteLine("2. Update Medicine Price. (price id)");
+            Console.WriteLine("3. Add Medicine.");
+            Console.WriteLine("4. Exit.");
+
+            while (flag)
+            {
+                try
+                {
+                    Console.WriteLine("Enter choice : ");
+                    string input = Console.ReadLine();
+                    string[] parts = input.Split(" ");
+                    int choice = int.Parse(parts[0]);
+                    switch (choice)
+                    {
+                        case 1:
+                            var result = mu.GetAllMedicines();
+                            foreach (var r in result)
+                            {
+                                Console.WriteLine($"Details : {r.Key} {r.Value.Name} {r.Value.Price} {r.Value.ExpiryYear}");
+                            }
+                            break;
+                        case 2:
+                            int newPrice = int.Parse(parts[1]);
+                            string id = parts[2];
+                            mu.UpdateMedicinePrice(id, newPrice);
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter Id : ");
+                            string Id = Console.ReadLine();
+                            Console.WriteLine("Enter Name : ");
+                            string Name = Console.ReadLine();
+                            Console.WriteLine("Enter Price : ");
+                            int Price = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter Expiry Year : ");
+                            int Year = int.Parse(Console.ReadLine());
+                            Medicine med = new Medicine()
+                            {
+                                Id = Id,
+                                Name = Name,
+                                Price = Price,
+                                ExpiryYear = Year
+                            };
+                            mu.AddMedicine(med);
+                            break;
+                        case 4:
+                            Console.WriteLine("Thank You");
+                            flag = false;
+                            return;
+                    }
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+            }
+
+        }
+    }
+
+}
---
+++ day31-PracticeQuestions/TechNova.cs
@@ -0,0 +1,161 @@
+﻿namespace TechNova
+{
+    // Delegate
+    public delegate void Notify();
+    public abstract class Category
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        public Category(string id, string name,decimal price, int qty)
+        {
+            if (string.IsNullOrWhiteSpace(name)) { throw new Exception("Name cannot be empty."); }
+            if (price < 0) { throw new Exception("Price cannot be negative."); }
+            Id = id;
+            Name = name;
+            Price = price;
+            Qty = qty;
+        }
+    }
+
+    public abstract class Electronics : Category
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int WarrantyPeriod { get; set; }
+        public double PowerUsage { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+
+        public Electronics(string id ,string name, decimal price,int qty,  string brand, string model, int warrantyPeriod, double powerUsage, DateTime manufacturingDate) : base(id,name,price,qty)
+        {
+            if (warrantyPeriod < 0 ) { throw new Exception("Warranty Period cannot be negative."); }
+            Brand = brand;
+            Model = model;
+            WarrantyPeriod = warrantyPeriod;
+            PowerUsage = powerUsage;
+            ManufacturingDate = manufacturingDate;
+        }
+    }
+
+    public class Laptop : Electronics
+    {
+        public int Ram { get; set; }
+        public int Storage { get; set; }
+
+        public Laptop(string id, string name, decimal price, int qty, string brand, string model, int warrantyPeriod, int powerUsage, DateTime manufacturingDate, int ram, int storage) : base(id, name, price,qty,brand,model,warrantyPeriod,powerUsage,manufacturingDate)
+        {
+            Ram = ram;
+            Storage = storage;
+        }
+    }
+
+    public abstract class Grocery : Category
+    {
+        public DateTime ExpiryDate { get; set; }
+        public double Weight { get; set; }
+        public bool isOrganic { get; set; }
+        public double StorageTemperature { get; set; }
+
+        public Grocery(string Id, string Name, decimal Price, int Qty, DateTime expiryDate, double weight, bool isOrganic, double storageTemperature) : base(Id, Name, Price, Qty)
+        {
+            if (weight < 0) { throw new Exception("Weight cannot be negative"); }
+            ExpiryDate = expiryDate;
+            Weight = weight;
+            this.isOrganic = isOrganic;
+            StorageTemperature = storageTemperature;
+        }
+    }
+
+    public class Milk : Grocery
+    {
+        public Milk(string Id, string Name, decimal Price, int Qty, DateTime expiryDate, double weight, bool isOrganic, double storageTemperature) : base(Id, Name, Price, Qty, expiryDate,weight,isOrganic,storageTemperature)
+        {
+            if (weight < 0) { throw new Exception("Weight cannot be negative"); }
+            ExpiryDate = expiryDate;
+            Weight = weight;
+            this.isOrganic = isOrganic;
+            StorageTemperature = storageTemperature;
+        }
+    }
+
+    public enum Gender { Men, Women, Unisex }
+    public enum Size { S, M, L, XL }
+
+
+    public abstract class Clothing : Category
+    {
+        public string FabricType { get; set; }
+        public Gender Gender { get; set; }
+        public string Color { get; set; }
+        public Size Size { get; set; }
+        public Clothing(string id, string name, decimal price, int qty, Size size, Gender gender, string fabricType, string color) : base(id, name, price, qty)
+        {
+            Size = size;
+            Gender = gender;
+            FabricType = fabricType;
+            Color = color;
+        }
+    }
+
+    public class Jeans : Clothing
+    {
+        public Jeans(string id, string name, decimal price, int qty, Size size, Gender gender, string fabricType, string color) : base(id, name, price, qty,size,gender,fabricType,color)
+        {
+            Size = size;
+            Gender = gender;
+            FabricType = fabricType;
+            Color = color;
+        }
+    }
+
+    public class Inventory
+    {
+        // Events
+        public event Notify ProductAdded;
+        public event Notify GettingProduct;
+
+        private List<Category> _products = new List<Category>();
+        public void AddProduct(Category product)
+        {
+            _products.Add(product);
+            ProductAdded?.Invoke();
+        }
+
+        public List<Category> GetProduct()
+        {
+            return _products.AsReadOnly().ToList();
+        }
+
+        public List<Electronics> GetElectronics()
+        {
+            return _products.OfType<Electronics>().ToList();
+        }
+
+        public List<T> GetProducts<T>() where T : Category
+        {
+            GettingProduct?.Invoke();
+            return _products.OfType<T>().ToList();
+        }
+    }
+
+    public class MainClass
+    {
+        // Methods for Event
+        public static void ProductAddedMessage() { Console.WriteLine("Product Added."); }
+        public static void GettingProductMessage() { Console.WriteLine("Getting Products...."); }
+        public static void Main(string[] args)
+        {
+
+            Inventory inventory = new Inventory();
+            inventory.ProductAdded += ProductAddedMessage;
+            inventory.GettingProduct += GettingProductMessage;
+            Laptop l = new Laptop("1", "Asus Tuf", 80000,100,"Asus", "Tuf", 24, 400, DateTime.ParseExact("21-12-2026","dd-MM-yyyy",null), 16, 1);
+            inventory.AddProduct(l);
+            foreach(var i in inventory.GetProducts<Laptop>())
+            {
+                Console.WriteLine(i.Id + i.Brand + i.Model + i.Price);
+            }
+        }
+    }
+}
